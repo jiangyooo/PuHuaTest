9855a2a4f2a24725d8ec7fe3d5530bd51f50a2f2
JiangBing <15333079013@163.com>
2023-03-11 19:19:10 +0800

    Initial commit

---
+++ src/main/java/com/jiang/puhuatest/PuHuaTestApplication.java
@@ -0,0 +1,13 @@
+package com.jiang.puhuatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuHuaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuHuaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiang/puhuatest/PuHuaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiang.puhuatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuHuaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
